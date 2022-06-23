82a1f2ddcc7394568f7b50f63a81e876f4ca48d8
Eugenio Regis <kuque84@hotmail.com>
2022-06-23 17:49:34 -0300

    y

--- js/Contacto.js
+++ js/Contacto.js
@@ -8,36 +8,32 @@ document.write(`
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
-<body>
+<body class="full">
     <section class="full">
         <div class="full-inner">
             <div class="content">
+                <h1 class="content">Contacto</h1>
+                <h2 class="parrafo">¡Dejanos tu Consulta!</h2>
+                <form id="contactform"> 
+                <h3 class="parrafo"><label for="name">Nombre</label></h3> 
+                    <input type="text" id=name name=name placeholder="Nombre y Apellido" required tabindex="1" /> 
+                    <h3 class="parrafo"><label for="email">e-mail</label></h3> 
+                    <input type="text" id=email name=email placeholder="example@domain.com" required tabindex="2" />                
+                    <h3 class="parrafo"><label for="comment">Tu Mensaje:</label></h3> 
+                    <textarea name="comment" id="comment" tabindex="4"></textarea>
+                    <br><br><br>
+                    <a href="index.html">Inicio</a>
+                    <a href="javascript:enviar()">Enviar Mensaje</a>
+                </form> 
             </div>
         </div>
     </section>
-
-<section id="contact"> <!-- HTML5 section tag for the contact 'section' -->
-        <h2 class="contact">Contact Me</h2>
-    <p>Alguna huevada en parrafo</p>
-    <form id="contactform"> 
-        <p><label for="name">Name</label></p> 
-        <input type="text" id=name name=name placeholder="First and last name" required tabindex="1" /> 
-        
-        <p><label for="email">Email</label></p> 
-        <input type="text" id=email name=email placeholder="example@domain.com" required tabindex="2" /> 
-        
-        <p><label for="comment">Your Message</label></p> 
-        <textarea name="comment" id="comment" tabindex="4"></textarea> 
-        
-        <input name="submit" type="submit" id="submit" tabindex="5" value="Send Message" /> 
-    </form> 
-</section>
+    <footer class="footer">
+        <span class="icono"> Grupo 8 </span><br>
+            &copy; Grupo 8 &reg;
+    </footer>
 </body>
-<footer class="footer">
-Grupo 8<br>
-Alan Abt - Ariel Berurena - Eugenio Regis<br>
-</footer>`)
-/* Fin finalizacion de estilo */
+`)
 }
