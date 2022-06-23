ab15c4a1a4ecf2ff89029bc511e0228c1933a6b7
Eugenio Regis <kuque84@hotmail.com>
2022-06-23 15:32:33 -0300

    y

--- js/Ejercicio2.js
+++ js/Ejercicio2.js
@@ -31,6 +31,30 @@ document.write(`
             </div>
         </div>
     </section>
+    /* ==============================CONTACTO============================== */
+    <section id="contact"> <!-- HTML5 section tag for the contact 'section' -->
+    	
+    <h2 class="contact">Contact Me</h2>
+    
+    <p>I whistled for a cab and when it came near the license plate said fresh and had dice in the mirror, if anything I could say that this cab was rare but I thought nah, <a href="">forget it</a>, yo home to Bel-Air! I pulled up to the house about seven or eight I yelled to the cabbie yo home, smell you later, looked at my kingdom I was finally there to sit on my throne as the prince of Bel-Air</p>
+    
+    <form id="contactform"> 
+
+        <p><label for="name">Name</label></p> 
+        <input type="text" id=name name=name placeholder="First and last name" required tabindex="1" /> 
+         
+        <p><label for="email">Email</label></p> 
+        <input type="text" id=email name=email placeholder="example@domain.com" required tabindex="2" /> 
+         
+        <p><label for="comment">Your Message</label></p> 
+        <textarea name="comment" id="comment" tabindex="4"></textarea> 
+         
+        <input name="submit" type="submit" id="submit" tabindex="5" value="Send Message" /> 
+         
+    </form> 
+
+
+</section>
 </body>
 <footer class="footer">
 Grupo 8<br>
